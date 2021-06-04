f28f8fd946d0d6bf45bcc1f0f379dfcca99d48dd
Neha121995 <jagtapneha1212@gmail.com>
2021-06-04 15:52:47 +0530

    Refactor: Display Unit,Ten,Hundred,Thousand

--- Selection-statement/if_elif/displayUnitTen.sh
+++ Selection-statement/if_elif/displayUnitTen.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -x
 
-read -p "Please give me a number" num
-if [ "$num" // "1000" ]
-then
-	echo "Thousand"
-elif [ "$num" // "100" ]
-then
-	echo "Hundred"
-elif [ "$num" // "10" ]
-then
-	echo "Ten"
-elif [ "$num" // "1" ]
+read -p "Please give me a number ::" num
+if [ $num -eq 1 ]
 then
 	echo "Unit"
+elif [ $num -eq 10 ]
+then
+	echo "Ten"
+elif [ $num -eq 100 ]
+then
+	echo "Hundred"
+elif [ $num -eq 1000 ]
+then
+	echo "Thousand"
 else
-	echo "Enter valid number"
+	echo "This is not valid number"
 fi
 
